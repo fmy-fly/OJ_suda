643fe76b85c9adb04148e47752996d034d2a6cf2
fmy <1791477899@qq.com>
2024-12-01 21:06:43 +0800

    前端页面导航栏创建

---
+++ Oj_frontend/src/layout/MenuLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="menu-demo">
+    <a-menu mode="horizontal" theme="dark" :default-selected-keys="['1']" class="menu">
+      <a-menu-item key="0" :style="{ padding: 0, marginRight: '38px' }" disabled>
+        <div>
+          <img src="../assets/suda.jpg" class="collegePicture" alt="Suda Image" />
+        </div>
+      </a-menu-item>
+      <a-menu-item key="1">主页</a-menu-item>
+      <a-menu-item key="2">题库</a-menu-item>
+      <a-menu-item key="3">讨论</a-menu-item>
+      <a-menu-item key="4">Cooperation</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<style scoped>
+.menu-demo {
+  box-sizing: border-box;
+  width: 100%;
+  background-color: var(--color-neutral-2);
+}
+
+.menu {
+  height: 60px; /* Set a fixed height for the menu */
+}
+
+.collegePicture {
+  height: 50px; /* Set a fixed height for the image */
+  width: auto;  /* Maintain the aspect ratio of the image */
+}
+</style>
--- Oj_frontend/src/main.ts
+++ Oj_frontend/src/main.ts
@@ -1,11 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import ArcoVue from '@arco-design/web-vue';
-import App from './App.vue';
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import ArcoVue from '@arco-design/web-vue'
 import '@arco-design/web-vue/dist/arco.css';
-import router from '@/router'
 
-const app = createApp(App);
-app.use(ArcoVue).use(router); // 链式调用, 装饰器模式  区别于建造者模式
-app.mount('#app');
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router).use(ArcoVue)
+app.mount('#app')
