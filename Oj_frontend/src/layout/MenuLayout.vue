bdfa8c16ba41e6dd5b098c2038f36903132bf1ca
fmy <1791477899@qq.com>
2024-12-04 18:07:57 +0800

    tab高亮

--- Oj_frontend/src/layout/MenuLayout.vue
+++ Oj_frontend/src/layout/MenuLayout.vue
@@ -1,24 +1,62 @@
 <template>
   <div class="menu-demo">
-    <a-menu mode="horizontal" theme="dark" :default-selected-keys="['1']" class="menu">
+    <a-menu
+      mode="horizontal"
+      theme="light"
+      class="menu"
+      :selected-keys="[route.path]"
+      @menu-item-click="doMenuClick"
+    >
       <a-menu-item key="0" :style="{ padding: 0, marginRight: '0' }" disabled>
         <div>
           <img src="../assets/suda.jpg" class="collegePicture" alt="Suda Image" />
         </div>
       </a-menu-item>
-      <a-menu-item key="1">主页</a-menu-item>
-      <a-menu-item key="2">题库</a-menu-item>
-      <a-menu-item key="3">讨论</a-menu-item>
-      <a-menu-item key="4">浏览题目</a-menu-item>
+      <a-menu-item class="SystemName">
+        <div id="suda">Suchow University Oj System</div>
+      </a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">
+        {{ item.name }}
+      </a-menu-item>
+
+      <a-input-search
+        class="searchLabel"
+        :style="{ width: '380px' }"
+        placeholder="Please enter something"
+        search-button
+      />
+
+      <a-menu-item class="user" key="4" style="font-size: 20px">注册或登录</a-menu-item>
     </a-menu>
   </div>
-  <router-view/>
+  <icon-notification />
+  <router-view />
 </template>
 <script setup lang="ts">
+import { routes } from '../router/routes.ts'
+import { useRoute, useRouter } from 'vue-router'
+const router = useRouter()
+//默认主页
+const route = useRoute()
 
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  })
+}
 </script>
 
 <style scoped>
+.SystemName {
+  margin-right: 10%;
+}
+
+#suda {
+  font-family: 'Roboto', sans-serif;
+  font-size: 30px;
+  font-weight: bold;
+}
+
 .menu-demo {
   box-sizing: border-box;
   width: 100%;
@@ -31,6 +69,14 @@
 
 .collegePicture {
   height: 50px; /* Set a fixed height for the image */
-  width: auto;  /* Maintain the aspect ratio of the image */
+  width: auto; /* Maintain the aspect ratio of the image */
+}
+
+.searchLabel {
+  margin-left: 6%;
+}
+
+.user {
+  margin: 0 auto;
 }
 </style>
